7e47c66f9324cdaef8494d58b4c8838e44834fe4
Nate Miller <nathanmiller9@yahoo.com>
2017-08-08 08:34:33 -0700

    changes to server script file for heroku deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(__dirname, '/public'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
